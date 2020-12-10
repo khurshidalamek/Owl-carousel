3ab5e140d3bb343ce6de39c5dbd620e50f029f38
khurshidalamek <75194655+khurshidalamek@users.noreply.github.com>
2020-12-10 09:24:32 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        loop:true,
+        margin:10,
+        nav:false,
+       
+
+    });
+  });
+
+
+
+
+ 
